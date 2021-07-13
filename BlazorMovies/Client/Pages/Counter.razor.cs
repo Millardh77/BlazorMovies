4905d68a7c97fc1740984973afcb7df5c826a191
Millardh77 <millard@freemountaininc.com>
2021-07-13 12:34:39 -0700

    Applying latest

--- BlazorMovies/Client/Pages/Counter.razor.cs
+++ BlazorMovies/Client/Pages/Counter.razor.cs
@@ -5,51 +5,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using static BlazorMovies.Client.Shared.MainLayout;
+using MathNet.Numerics.Statistics;
 
 namespace BlazorMovies.Client.Pages
 {
     public partial class Counter
     {
-        [Inject] SingletonService singleton { get; set; }
-        [Inject] TransientService transient { get; set; }
-        [Inject] IJSRuntime js { get; set; }
-
-        private List<Movie> movies;
-        protected override void OnInitialized()
-        {
-            movies = new List<Movie>()
-    {
-               new Movie(){Title = "Joker", ReleaseDate = new DateTime(2019, 7, 2)},
-               new Movie(){Title = "Avengers", ReleaseDate = new DateTime(2016, 11, 23)}
-            };
-        }
-
         private int currentCount = 0;
-        private static int currentCountStatic = 0;
-        IJSObjectReference module;
         
-        [JSInvokable]
-        public async Task IncrementCount()
+        public void IncrementCount()
         {
-            module = await js.InvokeAsync<IJSObjectReference>("import", "./js/Counter.js");
-            await module.InvokeVoidAsync("displayAlert", "hello world");
+            var array = new double[] { 1, 2, 3, 4, 5 };
+            var max = array.Maximum();
+            var min = array.Minimum();
+
+            Console.WriteLine($"Max is {max} and min is {min}");
+
             currentCount++;
-            singleton.Value += 1;
-            transient.Value += 1;
-            currentCountStatic++;
-            await js.InvokeVoidAsync("dotnetStaticInvocation");
-        }
-
-        private async Task IncrementCountJavaScript()
-        {
-            await js.InvokeVoidAsync("dotnetInstanceInvocation",
-                DotNetObjectReference.Create(this));
-        }
-
-        [JSInvokable]
-        public static Task<int> GetCurrentCount()
-        {
-            return Task.FromResult(currentCountStatic);
         }
     }
 }
